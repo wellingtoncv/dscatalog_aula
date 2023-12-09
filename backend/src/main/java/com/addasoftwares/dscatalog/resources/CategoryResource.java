b36a092dc92583a43c9e59c5c1a5af8a1b1a1b5f
Wellingtoncvs <wellington.santos@addasoftwares.com>
2023-12-09 19:57:37 -0300

    Post new Category

--- backend/src/main/java/com/addasoftwares/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/addasoftwares/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package com.addasoftwares.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.addasoftwares.dscatalog.dto.CategoryDTO;
 import com.addasoftwares.dscatalog.services.CategoryService;
@@ -32,6 +36,14 @@ public class CategoryResource {
 	public ResponseEntity<CategoryDTO> finById(@PathVariable Long id) {
 		CategoryDTO dto = service.findByid(id);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto){
+		dto = service.insert(dto);
+		URI uri= ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 
 }
