b19885bcdd236c6897d78d248669f55e134afc27
Wellingtoncvs <wellington.santos@addasoftwares.com>
2023-12-12 18:44:55 -0300

    Paginação

--- backend/src/main/java/com/addasoftwares/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/addasoftwares/dscatalog/services/CategoryService.java
@@ -1,11 +1,11 @@
 package com.addasoftwares.dscatalog.services;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -27,13 +27,22 @@ public class CategoryService {
 	private CategoryRepository repository;
 
 	// Garantias das propriedades ASID pelo framwork;
+	/*
+	 * @Transactional(readOnly = true) public List<CategoryDTO> findAll() {
+	 * List<Category> list = repository.findAll();
+	 * 
+	 * // utilizando a função de alta ordem e lambida return list.stream().map(x ->
+	 * new CategoryDTO(x)).collect(Collectors.toList());
+	 * 
+	 * }
+	 */
 
 	@Transactional(readOnly = true)
-	public List<CategoryDTO> findAll() {
-		List<Category> list = repository.findAll();
+	public Page<CategoryDTO> findAllPaged(PageRequest pageRequest) {
+		Page<Category> list = repository.findAll(pageRequest);
 
 		// utilizando a função de alta ordem e lambida
-		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		return list.map(x -> new CategoryDTO(x));
 
 	}
 
