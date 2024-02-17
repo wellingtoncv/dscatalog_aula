2840ba204af4a7e0ad7076d471290d59038048d2
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-02-17 18:53:33 -0300

    ORM mappin e database seeding

--- backend/src/main/java/com/addasoftwares/dscatalog/entities/User.java
+++ backend/src/main/java/com/addasoftwares/dscatalog/entities/User.java
@@ -5,16 +5,34 @@ import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String firstName;
 	private String lastName;
 	private String email;
 	private String password;
 	
+	// Construção da associação do relacionamento de muitos para muitos
+	
+	@ManyToMany
+	@JoinTable(name = "tb_user_role", 
+	joinColumns = @JoinColumn(name = "user_id"),
+	inverseJoinColumns = @JoinColumn(name = "role_id"))
 	private Set<Role> roles = new HashSet<>();	
 	
 	public User(){
