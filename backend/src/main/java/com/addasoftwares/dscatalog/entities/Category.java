6deafa97472895d5879a9b3c523bbe743ca398b1
Wellingtoncvs <wellington.santos@addasoftwares.com>
2024-02-17 18:01:55 -0300

    Implementação das entidades Role e User

--- backend/src/main/java/com/addasoftwares/dscatalog/entities/Category.java
+++ backend/src/main/java/com/addasoftwares/dscatalog/entities/Category.java
@@ -2,13 +2,16 @@ package com.addasoftwares.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
@@ -29,6 +32,9 @@ public class Category implements Serializable {
 	
 	@Column (columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	public Instant updatedAt;
+	
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
 	
 	public Category() {
 
@@ -71,6 +77,11 @@ public class Category implements Serializable {
 	@PreUpdate
 	public void preUpdate() {
 		updatedAt = Instant.now();
+	}
+	
+
+	public Set<Product> getProducts() {
+		return products;
 	}
 
 	@Override
